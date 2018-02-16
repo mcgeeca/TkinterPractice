09b9ad3a3840d3f124abe0a01283453f880a720c
mcgeeca <mcgeeca@pg.com>
2018-02-16 14:16:59 -0500

    DONE

--- src/m5_tkinter_practice.py
+++ src/m5_tkinter_practice.py
@@ -52,7 +52,7 @@ def main():
     hello_goodbye_button.grid()
 
     # ------------------------------------------------------------------
-    # TODO: 7.
+    # DONE: 7.
     #    -- Put a second Entry on the Frame.
     #    -- Put a third Button on the frame.
     #    -- Make this new Button respond to a button-press as follows:
